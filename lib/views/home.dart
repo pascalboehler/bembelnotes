3c66e4ad85af99e6abf1cff519efa11133bf8183
Pascal Böhler <pascal.boehler@hotmail.de>
2022-11-23 15:51:05 +0100

    feat: Renamed folder and fixed links in other classes

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,4 @@
-import "package:bembelnotes/mdRenderer/markdownrenderer.dart";
+import "package:bembelnotes/helper/markdownrenderer.dart";
 import "package:flutter/material.dart";
 
 class Home extends StatelessWidget {
